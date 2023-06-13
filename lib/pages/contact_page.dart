c6cb325d61656a122946c008bcf44f1ca4f5fecf
Kayouyou <yeyangyang519716@gmail.com>
2023-06-13 22:59:55 +0800

    modify

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_academy/res/responsive.dart';
 import 'package:flutter_academy/widgets/drawer_nav.dart';
 import 'package:flutter_academy/widgets/top_nav.dart';
 
+// 联系我们
 class ContactPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
