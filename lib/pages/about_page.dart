64df6046be7b1ffaad88e8d819e8c62696cdf350
Kayouyou <yeyangyang519716@gmail.com>
2023-06-13 22:25:09 +0800

    modify

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_academy/res/responsive.dart';
 import 'package:flutter_academy/widgets/drawer_nav.dart';
 import 'package:flutter_academy/widgets/top_nav.dart';
 
+// 关于 界面
 class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
